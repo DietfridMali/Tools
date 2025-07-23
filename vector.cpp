24e4d983ff011e886d64627af7c4ebbf792378c7
Dietfrid Mali <karx11erx@googlemail.com>
2025-07-23 14:33:51 +0200

    Add files via upload
    added std::string based string handling and vector code switcher file

--- vector.cpp
+++ vector.cpp
@@ -1,13 +1,9 @@
 #include "vector.hpp"
 
-const Vector3f Vector3f::ZERO = Vector3f({ 0.0f, 0.0f, 0.0f });
+#define COMPILE_VECTOR
 
-const Vector3f Vector3f::NONE = Vector3f({ NAN, NAN, NAN }); 
-
-const Vector3f Vector3f::ONE = Vector3f({ 1.0f, 1.0f, 1.0f });
-
-const Vector4f Vector4f::ZERO = Vector4f ({ 0.0f, 0.0f, 0.0f, 0.0f });
-
-const Vector4f Vector4f::ONE = Vector4f({ 1.0f, 1.0f, 1.0f, 1.0f });
-
-const Vector4f Vector4f::NONE = Vector4f ({ NAN, NAN, NAN, NAN });
+#if USE_GLM
+#	include "glm_vector.cpp"
+#else
+#	include "custom_vector.cpp"
+#endif
